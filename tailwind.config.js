61416fc7aac4285307c72ac38e273896c2962ee8
Zach Churchill <zachurchill@protonmail.com>
2024-08-13 13:32:20 -0400

    include tailwind with some initial styling

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+  },
+  variants: {},
+  plugins: [],
+}
