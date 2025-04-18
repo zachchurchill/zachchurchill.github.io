b805594628f693f934dae3869acf0dfb055a60f0
Zach Churchill <zachurchill@protonmail.com>
2025-04-18 08:43:56 -0400

    fixup bug related to path used to link to post

--- new_site/js/main.js
+++ new_site/js/main.js
@@ -1,17 +1,21 @@
-function loadPosts({ listId, jsonPath, topN = null }) {
+function loadPosts({ listId, pathPrefix = null, topN = null }) {
   const list = document.getElementById(listId);
   if (!list) {
     console.warn(`list element with ID "${listId}" not found`);
     return;
   }
 
+  const jsonPath = `${pathPrefix !== null ? pathPrefix : "./"}posts.json`;
   fetch(jsonPath)
     .then(res => res.json())
     .then(posts => {
       const postsToShow = topN !== null ? topN : posts.length;
       posts.slice(0, postsToShow).forEach(post => {
         const li = document.createElement("li");
-        li.innerHTML = `<span>${post.date}</span><a href="${post.path}">${post.name}</a>`;
+        li.innerHTML = `
+          <span>${post.date}</span>
+          <a href="${pathPrefix !== null ? pathPrefix : ""}${post.path}">${post.name}</a>
+        `;
         list.appendChild(li);
       });
     })
